5fdacfbb59e562f8e5136d03a9ee19ae06f2ca9c
Juliano Duarte <juliano.mimura.duarte@gmail.com>
2025-07-22 00:47:49 -0300

    nova condicao

--- Condicoes/condicao_aninhada/ex.js
+++ Condicoes/condicao_aninhada/ex.js
@@ -1,4 +1,16 @@
 var agora = new Date()
 var dia = agora.getDate()
+var mes = agora.getMonth()
 var hora = agora.getHours()
 var minutos = agora.getMinutes()
+
+console.log(`Hoje é dia ${dia} do mês ${mes}, são ${hora} hora e ${minutos} minutos`)
+if (hora < 12 && hora > 5) {
+    console.log(`Bom dia`)
+} else if (hora <= 17 && hora >= 12) {
+    console.log('Boa tarde')
+} else if (hora > 17 && hora < 0) {
+    console.log('Boa noite')
+} else {
+    console.log('Boa Madrugada')
+}
