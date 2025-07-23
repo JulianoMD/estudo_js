d4d7517203e6320a1d459e8bd6677fb7ed53c3de
Juliano Duarte <juliano.mimura.duarte@gmail.com>
2025-07-23 00:50:18 -0300

    coisa nova

--- DOM/DOM 1/script.js
+++ DOM/DOM 1/script.js
@@ -1,12 +1,13 @@
 var corpo = window.document.body
-corpo.style.background = "gray"
+corpo.style.background = "celest"
 var p1 = document.getElementsByTagName(`p`)[0] //Elements no plural deve se especificar qual é no []
 p1.style.background = 'blue'
 var d = document.querySelector('div.msg')
 d.style.background = 'red'
-d.innerText = "🐒 Troquei a frase no JavaScript 🦣"
+d.innerText = "🐒 Troquei a frase do html pelo JavaScript 🦣"
 document.write(`Troquei o texto da div do HTML, antes estava "Clique em mim", agora está escrita: ${d.innerText}<br>`)
 var app = (window.navigator.appName)
 var url = window.document.URL
 document.write(`O navegador usado é o: ${app}<br>`)
 document.write(`A url do site é: => ${url}<br>`)
+document.write(`estilo da pagina ficou um dos mais feios q já vi`)
