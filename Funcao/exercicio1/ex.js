55f73bd2c36187d7035007f2938349b4a7635d72
Juliano Duarte <juliano.mimura.duarte@gmail.com>
2025-07-26 20:53:01 -0300

    finalizacao curso em video

--- Funcao/exercicio1/ex.js
+++ Funcao/exercicio1/ex.js
@@ -3,42 +3,80 @@ let tab = document.querySelector('#seltab')
 let res = document.querySelector('#res')
 let vetor = []
 
-function adicionar() {
-    let num = +input_num.value
-    if (input_num.value.trim() === '') {
-        alert(`[ERRO] digite um valor valido`)
-        return
+function isNumero(n) {
+    if (n >= 1 && n <= 100) {
+        return true
+    } else {
+        return false
     }
-    if (num < 1 || num > 100) {
-        alert(`Digite um valor entre 1 e 100`)
-        return
+}
+function isLista(n, i) {
+    if (i.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
     }
-    if (vetor.includes(num)) {
+    /* if (vetor.includes(num)) {
         alert(`Numero já digitado`)
         return
+    } else {
+        return true
+    }
+    */
+}
+function isValido() {
+    if (input_num.value.trim() === '') {
+        return false
+    } else {
+        return true
+    }
+}
+
+function adicionar() {
+    let num = +input_num.value
+    if (isNumero(num) && !isLista(num, vetor) && isValido()) {
+        vetor.push(num)
+
+        let item = document.createElement('option')
+        item.text = `Valor ${num} salvo`
+        tab.appendChild(item)
+
+        input_num.value = ''
+        res.innerHTML = ''
+        input_num.focus()
+    } else {
+        alert(`Valor Inválido ou já digitado.`)
     }
 
-    vetor.push(num)
-
-    let item = document.createElement('option')
-    item.text = `Valor ${num} salvo`
-    tab.appendChild(item)
-
-    input_num.value = ''
-    res.innerHTML = ''
 }
 
 function finalizar() {
     if (vetor.length === 0) {
         alert('Digite ao menos um valor')
-        return
+    } else {
+        /* Usando sem as funçoes
+        let maior = vetor[0]
+        let menor = vetor[0]
+        let soma = 0
+        let media = 0
+        for (let pos in vetor) {
+            soma += vetor[pos]
+            if (pos > maior) {
+                maior = vetor[pos]
+            }
+            if (pos < menor) {
+                menor = vetor[0]
+            }
+        }
+        media = soma / vetor.length
+        */
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo temos ${vetor.length} numeros cadastrados.</p>`
+        res.innerHTML += `<p>O maior numero informado é ${maior(vetor)}.</p>`
+        res.innerHTML += `<p>O menor valor informado é ${menor(vetor)}.</p>`
+        res.innerHTML += `<p>A soma de todos os valores é ${soma(vetor)}.</p>`
+        res.innerHTML += `<p>A média dos valores é ${media(vetor)}`
     }
-    res.innerHTML = ''
-    res.innerHTML += `<p>Ao todo temos ${vetor.length} numeros cadastrados.</p>`
-    res.innerHTML += `<p>O maior numero informado é ${maior(vetor)}.</p>`
-    res.innerHTML += `<p>O menor valor informado é ${menor(vetor)}.</p>`
-    res.innerHTML += `<p>A soma de todos os valores é ${soma(vetor)}.</p>`
-    res.innerHTML += `<p>A média dos valores é ${media(vetor)}`
 }
 
 function maior(n) {
@@ -55,13 +93,11 @@ function soma(n) {
     return resultado
 }
 function media(n) {
-    media = 0
-    memory = 0
+    let soma = 0
     for (let i in n) {
-        memory = i + n[i]
+        soma += n[i]
     }
-    media = memory / n.length
-    return media
+    return soma / n.length
 }
 function recarregar() {
     location.reload()
